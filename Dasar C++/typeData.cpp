0c712d3781889b2a594467ef2a3f20ae971c3159
Dani170907 <daniramadhan170907@gmail.com>
2025-12-16 02:11:44 +0700

    update

---
+++ Dasar C++/typeData.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+   // Deklarasi semua tipe data
+   string nama = "Dani Ramadhan";
+   int umur = 18;
+   float tinggi = 170.5;
+   double berat = 50.55;
+   char grade = 'A';
+   bool lulus = true;
+
+   cout << "Nama: " << nama << endl;
+   cout << "Umur: " << umur << " tahun" << endl; 
+   cout << "Tinggi: " << tinggi << endl;
+   cout << "Berat: " << berat << endl;
+   cout << "Grade: " << grade << endl;
+   cout << "Lulus: " << lulus << endl;
+
+   return 0;
+}
