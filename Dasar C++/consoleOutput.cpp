8b3ceebb59b03213ca4683cab68fffc033492aeb
Dani170907 <daniramadhan170907@gmail.com>
2025-12-15 23:45:28 +0700

    update

--- Dasar C++/consoleOutput.cpp
+++ Dasar C++/consoleOutput.cpp
@@ -2,5 +2,12 @@
 using namespace std;
 
 int main () {
-   
+   int umur = 18;
+   string namaLengkap = "Dani Ramadhan";
+
+   cout << "Nama: " << namaLengkap << endl;
+   cout << "Umur" << umur << " Tahun" << endl;
+   cout << "Selamat datang di mata kuliah Dasar Pemrograman" << endl;
+
+   return 0;
 }
