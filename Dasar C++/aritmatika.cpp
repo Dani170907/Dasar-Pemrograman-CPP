234c20d705b3391df5bff7986abfd1f38171ccdd
Dani170907 <daniramadhan170907@gmail.com>
2025-12-16 07:08:14 +0700

    update

--- Dasar C++/aritmatika.cpp
+++ Dasar C++/aritmatika.cpp
@@ -2,5 +2,12 @@
 using namespace std;
 
 int main () {
-   
+   int a = 10, b = 3;
+   int hasil;
+
+   hasil = a + b;  // 13 (Penjumlahan)
+   hasil = a - b;  // 7  (Pengurangan)
+   hasil = a * b;  // 30 (Perkalian)
+   hasil = a / b;  // 3  (Pembagian integer)
+   hasil = a % b;  // 1  (Modulus/sisa bagi)
 }
