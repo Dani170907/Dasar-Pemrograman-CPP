578845a041e247a8191faf67f774c867e1809eb1
Dani170907 <daniramadhan170907@gmail.com>
2025-12-16 08:06:29 +0700

    update

---
+++ Fungsi/void.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// Deklarasi fungsi
+void salam() {
+   cout << "Halo, Selamat Datang" << endl;
+   cout << "Selamat belajar C++" << endl;
+}
+
+void cetakGaris() {
+   cout << "====================" << endl;
+}
+
+int main () {
+   salam();
+   cetakGaris();
+}
